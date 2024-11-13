650e489c88ea36ce5103a2fa7bf7d8f5a58e2ead
JohnTheWhiteBelt <john.the.white.belt@gmail.com>
2024-11-13 21:17:53 +0800

    implement setUp for test

--- test/PointsHook.t.sol
+++ test/PointsHook.t.sol
@@ -31,7 +31,43 @@ contract TestPointsHook is Test, Deployers {
 
 	PointsHook hook;
 
-	function setUp() public {
-		// TODO
-	}
+function setUp() public {
+    // Step 1 + 2
+    // Deploy PoolManager and Router contracts
+    deployFreshManagerAndRouters();
+
+    // Deploy our TOKEN contract
+    token = new MockERC20("Test Token", "TEST", 18);
+    tokenCurrency = Currency.wrap(address(token));
+
+    // Mint a bunch of TOKEN to ourselves
+    token.mint(address(this), 1000 ether);
+
+    // Deploy hook to an address that has the proper flags set
+    uint160 flags = uint160(
+        Hooks.AFTER_ADD_LIQUIDITY_FLAG | Hooks.AFTER_SWAP_FLAG
+    );
+    deployCodeTo(
+        "PointsHook.sol",
+        abi.encode(manager, "Points Token", "TEST_POINTS"),
+        address(flags)
+    );
+
+    // Deploy our hook
+    hook = PointsHook(address(flags));
+
+    // Approve our TOKEN for spending on the swap router and modify liquidity router
+    // These variables are coming from the `Deployers` contract
+    token.approve(address(swapRouter), type(uint256).max);
+    token.approve(address(modifyLiquidityRouter), type(uint256).max);
+
+    // Initialize a pool
+    (key, ) = initPool(
+        ethCurrency, // Currency 0 = ETH
+        tokenCurrency, // Currency 1 = TOKEN
+        hook, // Hook Contract
+        3000, // Swap Fees
+        SQRT_PRICE_1_1 // Initial Sqrt(P) value = 1
+    );
+}
 }
